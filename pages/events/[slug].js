08dec679c1634699270d7672533640c4873895b4
Irem Kenar <ikenar@kazandirio-dev.com>
2022-06-11 12:21:48 +0300

    Added pages and routing

---
+++ pages/events/[slug].js
@@ -0,0 +1,8 @@
+
+export default function EventPage() {
+    return (
+        <div>
+            This is my dynamic events page
+        </div>
+    )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,8 @@
+
+export default function AddEventPage() {
+    return (
+        <div>
+            Add Event Page
+        </div>
+    )
+}
